cdb0d4c92d66b42a2876bf96bd1c8a5f253732af
Alvaro94 <alvchasa_94@hotmail.es>
2016-06-26 21:36:40 -0600

    Tarea2
    Tareas Helpers

---
+++ tarea/tarea/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tarea.Startup))]
+namespace tarea
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
