353d20db9a15593db02ceacceeac0233a9765f6f
Alvaro94 <alvchasa_94@hotmail.es>
2016-06-27 21:20:41 -0600

    Quiz
    Quiz

---
+++ CRUD/CRUD/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace CRUD.Models
+{
+    public class Person
+    {
+        [Key]
+        public int id { get; set; }
+        public string Oficio { get; set; }
+        public string Empresa { get; set; }
+        public string Experiencia { get; set; }
+       
+    }
+}
---
+++ CRUD/CRUD/Models/PersonaDataContext .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace CRUD.Models
+{
+    public class PersonaDataContext : DbContext
+    {
+        public DbSet Person { get; set; }
+
+        public System.Data.Entity.DbSet<CRUD.Models.Person> People { get; set; }
+
+    }
+}
