fdc60574899761245362a59e7b52e335e1465f14
Alvaro94 <alvchasa_94@hotmail.es>
2016-08-10 23:45:24 -0600

    quiz
    comenzando

---
+++ CRUD/CRUD/Models/product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace crud.Models
+{
+    public class product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string category { get; set; }
+         public int price { get; set; }
+        
+    }
+
+    class ProductosDbContext: DbContext
+    {
+        public DbSet<product> product { get; set; }
+    }
+
+
+}
+
+
+
